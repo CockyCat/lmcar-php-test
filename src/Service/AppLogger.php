9cec175b6e68a96a75d56a17ec160e3ae3a40f53
Xu Gang <xugang@123.com>
2022-10-29 10:36:33 +0800

    LM test

--- src/Service/AppLogger.php
+++ src/Service/AppLogger.php
@@ -1,16 +1,102 @@
 <?php
 
 namespace App\Service;
+use think\facade\Log;
 
+interface LogBase {
+    public function info($message);
+    public function debug($message);
+    public function error($message);
+}
+
+class Log4php implements LogBase{
+    private $logger;
+
+
+    public function __construct(){
+        $this->logger = \Logger::getLogger("Log");
+    }
+
+    public function info($message = '')
+    {
+        $this->logger->info($message);
+    }
+
+    public function debug($message = '')
+    {
+        $this->logger->debug($message);
+    }
+
+    public function error($message = '')
+    {
+        $this->logger->debug($message);
+    }
+}
+
+class Thinklog implements LogBase{
+    private $logger;
+
+
+    public function __construct(){
+        Log::init([
+            'default'	=>	'file',
+            'channels'	=>	[
+                'file'	=>	[
+                    'type'	=>	'file',
+                    'path'	=>	'./logs/',
+                ],
+            ],
+        ]);
+    }
+
+    public function info($message = '')
+    {
+
+        Log::info(strtoupper($message));
+    }
+
+    public function debug($message = '')
+    {
+        Log::debug(strtoupper($message));
+
+    }
+
+    public function error($message = '')
+    {
+        Log::error(strtoupper($message));
+
+    }
+}
+class LogFactory{
+    public static function build($className){
+        $className = ucfirst($className);
+        if ($className && class_exists($className)){
+            return new $className();
+        }
+        return null;
+    }
+}
+
+//call log factory
+LogFactory::build("Thinklog")->info("Products was sell out!");
+LogFactory::build("Log4php")->error("mysql connection was broke!");
+
+
+
+/*
 class AppLogger
 {
     const TYPE_LOG4PHP = 'log4php';
+    const TYPE_THINKLOG = 'think-log';
 
     private $logger;
 
     public function __construct($type = self::TYPE_LOG4PHP)
     {
         if ($type == self::TYPE_LOG4PHP) {
+            $this->logger = \Logger::getLogger("Log");
+        }
+        if ($type == self::TYPE_THINKLOG) {
             $this->logger = \Logger::getLogger("Log");
         }
     }
@@ -30,3 +116,4 @@ class AppLogger
         $this->logger->error($message);
     }
 }
+*/
--- src/Service/ProductHandler.php
+++ src/Service/ProductHandler.php
@@ -4,5 +4,40 @@ namespace App\Service;
 
 class ProductHandler
 {
+    public static function calculatePrice($products){
+        $totalPrice = 0;
+        if (!count($products) > 0){
+            return $totalPrice;
+        }
+        foreach ($products as $product) {
+            $price = $product['price'] ?: 0;
+            $totalPrice += $price;
+        }
+        return $totalPrice;
+    }
 
+    public static function sortByPrice($products){
+        if (!count($products) > 0){
+            return array();
+        }
+        //filted type acccording by type
+        foreach ($products as $key => $product){
+            if ($product['type'] != "Dessert"){
+                unset($products[$key]);
+            }
+        }
+        array_multisort(array_column($products, 'price'),SORT_DESC,$products);
+        return $products;
+    }
+
+    public static function convertDateToTimestamp($products){
+        if (!count($products) > 0){
+            return array();
+        }
+        //filted type acccording by type
+        foreach ($products as $key => $product){
+            $products[$key]["create_at_timestamp"] = strtotime($product["create_at"]);
+        }
+        return $products;
+    }
 }
